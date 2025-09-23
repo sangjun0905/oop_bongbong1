e00e1e76f0ab0fe385225764616469d57d4cbbb9
jwoobin7372 <jwoobin7372@gmail.com>
2025-09-23 21:34:46 +0900

    Create InsertionController.cpp

---
+++ InsertionController.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+class InsertionController
+{
+private:
+    /* data */
+    //StudentInfoModel studim;
+public:
+    InsertionController(/* args */);
+    ~InsertionController();
+    void insert();
+};
+
+InsertionController::InsertionController(/* *StudentInfomodel studimp */)
+{
+}
+
+InsertionController::~InsertionController()
+{
+}
+
+void InsertionController::insert()
+{
+    // Insertion View 출력
+    // 이름 입력받고 적절한지 확인
+    // 학번 입력받고 적절한지 확인
+    // 학과 입력받고 적절한지 확인
+    // 전화번호 입력받고 적절한지 확인
+}
