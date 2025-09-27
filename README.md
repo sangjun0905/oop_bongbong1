86ee15cafb2c64e96b0b886057b444a90408a4ba
hanbin5 <hanbinftr55@gmail.com>
2025-09-27 22:33:11 +0900

    MainMenuView

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # oop_bongbong1
+봉봉 과제1 proj
 
-봉봉 과제1 prob2
-
-mkdir build && cd build
-cmake ..
-make
+mkdir build && cd build 
+cmake .. 
+make 
 ./StudentInfoSystem file1.txt
--- src/MainMenuView.cpp
+++ src/MainMenuView.cpp
@@ -2,7 +2,29 @@
 
 class MainMenuView {
 public:
-    void display() {
-        std::cout << "Main menu" << std::endl;
+    char display() {
+        char input[10];
+        
+        do {
+            std::cout << "1. Insertion" << std::endl;
+            std::cout << "2. Search" << std::endl;
+            std::cout << "3. Sorting Option" << std::endl;
+            std::cout << "4. Exit" << std::endl;
+            std::cout << "> ";
+            
+            std::cin >> input;
+        } while (check_input(input));
+
+        return input[0];
+    }
+
+    int check_input(char* input) {
+        if (input[0] == '1') return 0;
+        if (input[0] == '2') return 0;
+        if (input[0] == '3') return 0;
+        if (input[0] == '4') return 0;
+
+        std::cout << "Try again!" << std::endl;
+        return 1;
     }
 };
