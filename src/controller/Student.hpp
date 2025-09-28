ce01201705ec93290397ed2ece0842c5f5c5f318
02juwon <102983484+02juwon@users.noreply.github.com>
2025-09-29 00:51:39 +0900

    search

--- src/controller/Student.hpp
+++ src/controller/Student.hpp
@@ -1,4 +1,4 @@
-#pragma once
+/*#pragma once
 #include <string>
 
 using namespace std;
@@ -10,4 +10,26 @@ struct student {
     int birthyear;
     char department[64];
     char tel[24];
+};*/
+
+#ifndef STUDENT_HPP 
+#define STUDENT_HPP
+
+class Student {
+public :   
+    char name[16];
+    int studentID;
+    int birthyear;
+    char department[64];
+    char tel[24];
+    
+    std::string getName() const { /* ... */ };
+    std::string getStudentID() const { /* ... */ };
+    std::string getBirthYear() const { /* ... */ };
+    std::string getDepartment() const { /* ... */ };
+    std::string getTel() const { /* ... */ };
+    std::string getAllStudents();
 };
+
+#endif
+
