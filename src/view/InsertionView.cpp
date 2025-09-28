64cfd8ed1bd98f4c5a6d33dc2633f84c76bf0dc5
hanbin5 <hanbinftr55@gmail.com>
2025-09-28 19:50:14 +0900

    InsertionView encapsulate

--- src/view/InsertionView.cpp
+++ src/view/InsertionView.cpp
@@ -15,40 +15,35 @@ public:
     char* getDepartment() { return inputDepartment; }
     char* getTel() { return inputTel; }
 
-    char* setName() {
+    void setName() {
         do {
             std::cout << "Name: ";
             std::cin >> inputName;
         } while (checkName(inputName));
-        return inputName;
     }
-    char* setStudentID() {    
+    void setStudentID() {    
         do {
             std::cout << "StudentID: ";
             std::cin >> inputStudentID;
         } while (checkName(inputStudentID));
-        return inputStudentID;
     }
-    char* setBirthYear() {    
+    void setBirthYear() {    
         do {
             std::cout << "BirthYear: ";
             std::cin >> inputBirthYear;
         } while (checkBirthYear(inputBirthYear));
-        return inputBirthYear;
     }
-    char* setDepartment() {
+    void setDepartment() {
         do {
             std::cout << "Department: ";
             std::cin >> inputDepartment;
         } while (checkDepartment(inputDepartment));
-        return inputDepartment;
     }
-    char* setTel() {
+    void setTel() {
         do {
             std::cout << "Tel: ";
             std::cin >> inputTel;
         } while (checkTel(inputTel));
-        return inputTel;
     }
     
     int checkName(char* inputName) {
