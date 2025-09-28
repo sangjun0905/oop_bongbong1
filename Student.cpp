4240c074c9ab7617d6c96a9dd6cbf0ce351db942
tjddn1851 <102117722+tjddn1851@users.noreply.github.com>
2025-09-28 21:59:01 +0900

    학생클래스

--- Student.cpp
+++ Student.cpp
@@ -1,13 +1,55 @@
-#include "Student.hpp"
-#include <stdexcept>
-#include <sstream>
-#include <string>
+#include<cstring>
+#include<string>
 
-Student::Student(const std::string& name, const std::string& stdudentId, const std::string& department, const std::string& tel){
+class Student{
+    private:
+        char name[16];
+        int s_id;
+        char tel[24];
+        int birth;
+        char department[64];
+    
+    public:
 
-    if(name.empty()||studentId.empty()){
-        throw std::invalid_argument("Name and Student ID cannot be blank.");
-    }
+        Student(const char* n, int id, const char* t, int b, const char* d) : s_id(id), birth(b){
 
-    if()
-}
+            std::strncpy(name, n, 15);
+            name[15] = '\0';
+
+            std::strncpy(department, d, 63);
+            department[63] = '\0';
+
+            std::strncpy(tel, t, 24);
+            tel[24] = '\0';
+
+        }
+        const char* get_name() const { return name; }
+        const int get_s_id() const { return s_id; }
+        const char* get_tel() const { return tel; }
+        const int get_birth() const { return birth; }
+        const char* get_dept() const { return department; }
+        
+        void set_name(const char* n){
+            std::strncpy(name,n, 15);
+            name[15] = '\0';
+        }
+
+        void set_s_id(int id){
+            s_id = id;
+        }
+
+        void set_tel(const char* t) {
+            std::strncpy(tel, t, 23);
+            tel[23] = '\0';
+        }
+
+        void set_birth(int b) {
+            birth = b;
+        }
+
+        void set_dept(const char* d) {
+            std::strncpy(department, d, 63);
+            department[63] = '\0';
+        }
+
+};
