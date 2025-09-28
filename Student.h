a6c99c223d65978bdb8078759cf96a890519df10
tjddn1851 <102117722+tjddn1851@users.noreply.github.com>
2025-09-29 08:31:15 +0900

    학생클래스 id char변경

--- Student.h
+++ Student.h
@@ -1,34 +1,37 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include <cstring>   // strncpy 사용을 위해 필요
-#include <string>    // std::string 사용을 위해 필요
+#include <cstring>  
+#include <string>   
 
 
 class Student {
 private:
    
-    char name[16];
-    int s_id;
-    char tel[24];
+    char name[17];
+    char s_id[9];
+    char tel[25];
     int birth;
-    char department[64];
+    char department[65];
 
 public:
     // 생성자
-    Student(const char* n, int id, const char* t, int b, const char* d)
+    Student(const char* n, const char* id, const char* t, int b, const char* d)
         // int은 초기화 리스트에서 처리
-        : s_id(id), birth(b) 
+        : birth(b) 
     {
-        // char 배열은 생성자 본문에서 strncpy로 처리
-        std::strncpy(name, n, 15);
-        name[15] = '\0'; // 널 종료 보장
+        
+        std::strncpy(name, n, 16);
+        name[16] = '\0'; // 널 종료 보장
 
-        std::strncpy(department, d, 63);
-        department[63] = '\0';
+        std::strncpy(department, d, 64);
+        department[64] = '\0';
+
+        std::strncpy(s_id, id, 8);
+        s_id[8] = '\0';   
 
         std::strncpy(tel, t, 24);
-        tel[23] = '\0';
+        tel[24] = '\0';
         
     }
 
@@ -37,7 +40,7 @@ public:
     
     const char* get_name() const { return name; } 
    
-    int get_s_id() const { return s_id; } 
+    const char* get_s_id() const { return s_id; } 
     
     const char* get_tel() const { return tel; } 
 
@@ -46,31 +49,33 @@ public:
     const char* get_department() const { return department; }
 
     void set_name(const char* n) {
-        std::strncpy(name, n, 15);
-        name[15] = '\0';
+        std::strncpy(name, n, 16);
+        name[16] = '\0';
     }
 
-    // s_id Setter
-    void set_s_id(int id) {
-        s_id = id;
+ 
+    
+    void set_s_id(const char* id){
+        std::strncpy(s_id, id, 8);
+        s_id[8] = '\0';  
     }
 
-    // tel Setter: 문자열 복사 (최대 23자 + '\0')
+   
     void set_tel(const char* t) {
-        std::strncpy(tel, t, 23);
-        tel[23] = '\0';
+        std::strncpy(tel, t, 24);
+        tel[24] = '\0';
     }
 
-    // birth Setter
+ 
     void set_birth(int b) {
         birth = b;
     }
 
-    // department Setter: 문자열 복사 (최대 63자 + '\0')
+    
     void set_department(const char* d) {
-        std::strncpy(department, d, 63);
-        department[63] = '\0';
+        std::strncpy(department, d, 64);
+        department[64] = '\0';
     }
 };
 
-#endif // STUDENT_HPP
+#endif
