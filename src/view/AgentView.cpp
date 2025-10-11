8ae6b1ef8103b06d29e8391277a0a4d3bcb497c9
juwon <02juwon@naver.com>
2025-10-11 12:08:30 +0900

    AgentView

---
+++ src/view/AgentView.cpp
@@ -0,0 +1,10 @@
+#include "AgentView.hpp"
+
+std::string AgentView::getdisplay() {
+    
+    std::string input;
+
+    std::getline(std::cin, input);
+
+    return input;
+};
---
+++ src/view/AgentView.hpp
@@ -0,0 +1,12 @@
+#ifndef AGENTVIEW_HPP
+#define AGENTVIEW_HPP
+
+#include <iostream>
+#include <string>
+
+class AgentView {
+public : 
+    std::string getdisplay();
+};
+
+#endif
